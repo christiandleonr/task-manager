5495ea9e2d14f604e2eee9e173d4facef6ae6e01
Christian Ramirez <christiandleonr@gmail.com>
2020-09-13 14:12:39 -0600

    Adding field name to Task model

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const taskSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
     description: {
         type: String,
         required: true,
