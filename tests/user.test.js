8e6d9503844902678f5f16ff73f9383460a7cabc
Christian Ramirez <christiandleonr@gmail.com>
2020-09-22 16:39:50 -0600

    Adding test

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,8 +3,15 @@ const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: 'kiensave'
+}
+
 beforeEach(async () => {
     await User.deleteMany()
+    await new User(userOne).save()
 })
 
 test('Signup a new user', async () => {
@@ -16,4 +23,14 @@ test('Signup a new user', async () => {
             password: 'kiensave'
         })
         .expect(201)
+})
+
+test('Login existing user', async () => {
+    await request(app)
+        .post('/users/login')
+        .send({
+            email: userOne.email,
+            password: userOne.password
+        })
+        .expect(200)
 })
