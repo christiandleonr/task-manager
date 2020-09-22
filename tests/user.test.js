786e33aa609fc5c38270fd73fde8acc917e30f04
Christian Ramirez <christiandleonr@gmail.com>
2020-09-22 16:18:29 -0600

    Cleaning database before test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,11 @@
 const { TestScheduler } = require('jest')
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+beforeEach(async () => {
+    await User.deleteMany()
+})
 
 test('Signup a new user', async () => {
     await request(app)
